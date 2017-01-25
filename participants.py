4ec3199d43eb3333c10f106ef739fc564b18a157
Will Brodnax <wbrodnax@gmail.com>
2017-01-25 09:30:49 -0500

    add separate module for participants

---
+++ participants.py
@@ -0,0 +1,58 @@
+#this file contains data about the current participants of lunch roulette
+
+people = {
+    'p1' : {
+        'first-name' : 'Amy',
+        'last-name' : 'Mason',
+        'nickname' : '',
+        'respin' : {
+            'available' : True,
+            'data-used' : null
+        }
+    },
+    'p2' : {
+        'first-name' : 'Larry',
+        'last-name' : 'Ball',
+        'nickname' : 'The Delicate Lunch Flower',
+        'respin' : {
+            'available' : True,
+            'data-used' : null
+        }
+    },
+    'p3' : {
+        'first-name' : 'Leah',
+        'last-name' : 'Wilson',
+        'nickname' : '',
+        'respin' : {
+            'available' : True,
+            'data-used' : null
+        }
+    },
+    'p4' : {
+        'first-name' : 'Braden',
+        'last-name' : 'Turner',
+        'nickname' : 'The Aussie',
+        'respin' : {
+            'available' : True,
+            'data-used' : null
+        }
+    },
+    'p5' : {
+        'first-name' : 'Brian',
+        'last-name' : 'Cooper',
+        'nickname' : 'The Bug Squasher',
+        'respin' : {
+            'available' : True,
+            'data-used' : null
+        }
+    },
+    'p6' : {
+        'first-name' : 'Will',
+        'last-name' : 'Brodnax',
+        'nickname' : '',
+        'respin' : {
+            'available' : True,
+            'data-used' : null
+        }
+    }
+}
